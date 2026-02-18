2538153af8a122122e0268cb349edd1a8b8a0079
kefanshi-design <kefanshiart@gmail.com>
2026-02-18 18:09:53 -0500

    Deleted Analysis Code

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -27,7 +26,6 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
-        <Analytics />
       </body>
     </html>
   );
